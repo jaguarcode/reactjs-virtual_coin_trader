ba43a042ea1298183f64098d72fe6baa1654e817
IKHYEON KIM <behonz@gmail.com>
2021-04-22 19:51:02 +0900

    Firebase 연결 설정 (추후 버전 맞춤 필요)

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+const isDev = process.env.NODE_ENV === 'development';
 const Api = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: isDev ? 'http://localhost:4000/' : '/api/',
 });
 
 export default Api;
